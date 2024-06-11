bb941fd1de1e0664533fbbba155f242eaed5feb6
Jake Roh <enfpdev@gmail.com>
2024-06-12 06:04:18 +0900

    Rename

--- simple-test.js
+++ simple-test.js
@@ -1,5 +1,22 @@
-const { plus100 } = require('./index')
-
-console.assert(plus100(0) === 100, 'Simple test failed')
-
-console.info('Simple test passed')
+import {
+    moveMouseRel,
+    moveMouseAbs,
+    mouseClick,
+    mouseDown,
+    mouseUp,
+    mouseScroll,
+  } from "./index.js";
+  
+  const x = 100;
+  const y = 100;
+  const button = 'right';
+  const length = 100;
+  const isVertical = true;
+  
+  console.log(moveMouseRel(x, y));
+  console.log(moveMouseAbs(x, y));
+  console.log(mouseClick(button));
+  console.log(mouseDown(button));
+  console.log(mouseUp(button));
+  console.log(mouseScroll(length, isVertical));
+  

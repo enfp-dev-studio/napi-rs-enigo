5582c584b54abe7b5c2318964857e94c12a41931
Jake Roh <enfpdev@gmail.com>
2024-06-12 16:41:02 +0900

    update

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,25 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { moveMouseRel, moveMouseAbs, mouseClick, mouseDown, mouseUp, mouseScroll } from '../index'
 
 test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+  const x = 100
+  const y = 100
+  const button = 'right'
+  const length = 100
+  const isVertical = true
+
+  // console.log(moveMouseRel(x, y))
+  // console.log(moveMouseAbs(x, y))
+  // console.log(mouseClick(button))
+  // console.log(mouseDown(button))
+  // console.log(mouseUp(button))
+  // console.log(mouseScroll(length, isVertical))
+
+  t.is(moveMouseRel(x, y), undefined)
+  t.is(moveMouseAbs(x, y), undefined)
+  t.is(mouseClick(button), undefined)
+  t.is(mouseDown(button), undefined)
+  t.is(mouseUp(button), undefined)
+  t.is(mouseScroll(length, isVertical), undefined)
 })

8766825ef96e1188d54cbd43f4dc7c7ec2d662ea
Jake Roh <enfpdev@gmail.com>
2024-06-13 14:16:17 +0900

    install

--- index.js
+++ index.js
@@ -62,9 +62,7 @@ switch (platform) {
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(
-          join(__dirname, 'napi-rs-enigo.win32-x64-msvc.node')
-        )
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.win32-x64-msvc.node'))
         try {
           if (localFileExisted) {
             nativeBinding = require('./napi-rs-enigo.win32-x64-msvc.node')
@@ -76,9 +74,7 @@ switch (platform) {
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(
-          join(__dirname, 'napi-rs-enigo.win32-ia32-msvc.node')
-        )
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.win32-ia32-msvc.node'))
         try {
           if (localFileExisted) {
             nativeBinding = require('./napi-rs-enigo.win32-ia32-msvc.node')
@@ -90,9 +86,7 @@ switch (platform) {
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(
-          join(__dirname, 'napi-rs-enigo.win32-arm64-msvc.node')
-        )
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.win32-arm64-msvc.node'))
         try {
           if (localFileExisted) {
             nativeBinding = require('./napi-rs-enigo.win32-arm64-msvc.node')
@@ -131,9 +125,7 @@ switch (platform) {
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(
-          join(__dirname, 'napi-rs-enigo.darwin-arm64.node')
-        )
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.darwin-arm64.node'))
         try {
           if (localFileExisted) {
             nativeBinding = require('./napi-rs-enigo.darwin-arm64.node')
@@ -167,9 +159,7 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'napi-rs-enigo.linux-x64-musl.node')
-          )
+          localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.linux-x64-musl.node'))
           try {
             if (localFileExisted) {
               nativeBinding = require('./napi-rs-enigo.linux-x64-musl.node')
@@ -180,9 +170,7 @@ switch (platform) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'napi-rs-enigo.linux-x64-gnu.node')
-          )
+          localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.linux-x64-gnu.node'))
           try {
             if (localFileExisted) {
               nativeBinding = require('./napi-rs-enigo.linux-x64-gnu.node')
@@ -196,9 +184,7 @@ switch (platform) {
         break
       case 'arm64':
         if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'napi-rs-enigo.linux-arm64-musl.node')
-          )
+          localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.linux-arm64-musl.node'))
           try {
             if (localFileExisted) {
               nativeBinding = require('./napi-rs-enigo.linux-arm64-musl.node')
@@ -209,9 +195,7 @@ switch (platform) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'napi-rs-enigo.linux-arm64-gnu.node')
-          )
+          localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.linux-arm64-gnu.node'))
           try {
             if (localFileExisted) {
               nativeBinding = require('./napi-rs-enigo.linux-arm64-gnu.node')
@@ -224,9 +208,7 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(
-          join(__dirname, 'napi-rs-enigo.linux-arm-gnueabihf.node')
-        )
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-enigo.linux-arm-gnueabihf.node'))
         try {
           if (localFileExisted) {
             nativeBinding = require('./napi-rs-enigo.linux-arm-gnueabihf.node')
